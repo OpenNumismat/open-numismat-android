59ffa7e63c30a2a8fa634196acb14673148a1d7f
Janis <ignatov.vit@gmail.com>
2014-10-23 18:08:54 +0400

    Removed unused UI items

--- app/src/main/java/janis/opennumismat/MainActivity.java
+++ app/src/main/java/janis/opennumismat/MainActivity.java
@@ -175,12 +175,9 @@ public class MainActivity extends Activity
     public void onSectionAttached(int number) {
         switch (number) {
             case 1:
-                mTitle = getString(R.string.title_section1);
+                mTitle = getString(R.string.app_name);
                 break;
             case 2:
-                mTitle = getString(R.string.title_section2);
-                break;
-            case 3:
                 mTitle = getString(R.string.title_section_about);
                 break;
         }
--- app/src/main/java/janis/opennumismat/NavigationDrawerFragment.java
+++ app/src/main/java/janis/opennumismat/NavigationDrawerFragment.java
@@ -103,7 +103,6 @@ public class NavigationDrawerFragment extends Fragment {
                 android.R.id.text1,
                 new String[]{
                         getString(R.string.title_section1),
-                        getString(R.string.title_section2),
                         getString(R.string.title_section_about),
                 }));
         mDrawerListView.setItemChecked(mCurrentSelectedPosition, true);
@@ -244,11 +243,6 @@ public class NavigationDrawerFragment extends Fragment {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         if (mDrawerToggle.onOptionsItemSelected(item)) {
-            return true;
-        }
-
-        if (item.getItemId() == R.id.action_example) {
-            Toast.makeText(getActivity(), "Example action.", Toast.LENGTH_SHORT).show();
             return true;
         }
 
